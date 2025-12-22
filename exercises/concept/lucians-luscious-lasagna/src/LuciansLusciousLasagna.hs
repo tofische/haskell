9719d26dcabf701986d417441a7394f72c43fb95
tofische <tofische@users.noreply.github.com>
2025-12-22 09:28:26 +0100

    Correct failed build.

--- exercises/concept/lucians-luscious-lasagna/src/LuciansLusciousLasagna.hs
+++ exercises/concept/lucians-luscious-lasagna/src/LuciansLusciousLasagna.hs
@@ -1,7 +1,13 @@
 module LuciansLusciousLasagna (elapsedTimeInMinutes, expectedMinutesInOven, preparationTimeInMinutes) where
 
 -- TODO: define the expectedMinutesInOven constant
+expectedMinutesInOven :: Int
+expectedMinutesInOven = undefined
 
 -- TODO: define the preparationTimeInMinutes function
+preparationTimeInMinutes :: Int -> Int
+preparationTimeInMinutes = undefined
 
 -- TODO: define the elapsedTimeInMinutes function
+elapsedTimeInMinutes :: Int -> Int -> Int
+elapsedTimeInMinutes = undefined
--- exercises/practice/leap/.meta/examples/error-nosig/src/LeapYear.hs
+++
@@ -1,4 +0,0 @@
-module LeapYear (isLeapYear) where
-
-isLeapYear year = hasFactor 4 && (not (hasFactor 100) || hasFactor 400)
-  where hasFactor n = year `rem` n == 0
